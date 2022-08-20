a16cc846ea609957be0143fac4241dd3dd98aa8e
denqueaz06 <dquesada@gticr.com>
2022-08-20 16:48:42 -0600

    Utilizando Router dom

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '../containers/Layout'
 import Login from '../containers/Login'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
 import RecoveryPassword from '../containers/RecoveryPassword'
 import '../styles/global.css'
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
       <Layout>
-        
-        <Route exact path="/" element={ <Home /> } />
-        <Route exact path="/login" element={ <Login /> } />
-        <Route exact path="/recovery-password" element={ <RecoveryPassword /> } />  
-        <Route element={ <NotFound /> } />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Layout>
-    </Routes>
     </BrowserRouter>
   )
 }
